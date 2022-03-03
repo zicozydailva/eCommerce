7a3b07c9ec0ca9f9158f4528832afd953678a8e1
Ogbu Ezekiel Ike <zico@zico.local>
2022-03-03 13:54:53 +0100

    newsletter setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Categories from './components/Categories';
-import Products from './components/Products';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
      <Home />
-     <Categories />
-     <Products />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Announcement from '../components/Announcement'
-import Navbar from '../components/Navbar'
-import Slider from '../components/Slider'
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Navbar from "../components/Navbar";
+import NewsLetter from "../components/NewsLetter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-        <Announcement />
-        <Navbar />
-        <Slider />
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+      <NewsLetter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
